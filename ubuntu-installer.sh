eaa59ea03763bc388358803b2ab76e7471b827d4
Juangr4 <48179884+Juangr4@users.noreply.github.com>
2022-10-18 02:34:51 +0200

    ubuntu-installer
    Corregido fallo al crear base de postgresql

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -5,17 +5,15 @@
 # Actualizamos la lista de indices
 sudo apt update
 
-# sudo add-apt-repository ppa:deadsnakes/ppa
 # Paquetes necesarios para el proyecto
+# sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.8-dev python3.8-venv postgresql gcc chromium-browser chromium-chromedriver -y
 sudo systemctl enable postgresql
 
 # Creacion de usuario y base de datos
-sudo su - postgres
-psql -c "create user decide with password 'decide'"
-psql -c "create database decide owner decide"
-psql -c "alter user decide createdb;"
-exit
+sudo su - postgres -c "psql -c \"create user decide with password 'decide'\""
+sudo su - postgres -c "psql -c \"create database decide owner decide\""
+sudo su - postgres -c "psql -c \"alter user decide createdb;\""
 
 # Crear y activar entorno
 python3.8 -m venv .env
